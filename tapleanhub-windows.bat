af9cb68fa1243a55fa7e97e3f53bd6e3e01faeff
Yuri Gomes <73554025+YuriGomes4@users.noreply.github.com>
2023-10-13 20:48:58 -0300

    Update exec

--- tapleanhub-windows.bat
+++ tapleanhub-windows.bat
@@ -1,4 +1,10 @@
 @echo off
 
+REM Executar a instala  o dos pacotes em segundo plano (sem janela vis vel)
+start /B %~dp0\wvenv\Scripts\python.exe -m pip install -r %~dp0\requirements.txt
+
 REM Executar o script Python em segundo plano (sem janela vis vel)
 start /B %~dp0\wvenv\Scripts\python.exe %~dp0\run.py
+
+REM Mantenha o prompt de comando aberto para visualiza  o (opcional)
+cmd /k
