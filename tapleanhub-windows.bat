c8ec783dc97505bc118858d8213d725f9b30635f
Yuri Gomes <73554025+YuriGomes4@users.noreply.github.com>
2023-10-13 20:31:51 -0300

    Windows bat

--- tapleanhub-windows.bat
+++ tapleanhub-windows.bat
@@ -2,5 +2,5 @@
 REM Ativar o ambiente virtual
 call .\wvenv\Scripts\activate
 
-REM Executar o script Python
-python run.py
+REM Executar o script Python em segundo plano (sem janela vis vel)
+start /B python run.py
